00f78c21d03f57f00d65f9656afa31e8d6626eb4
Anthony Perritano <aperritano@gmail.com>
2020-02-04 08:13:31 -0600

    trying to get run information from within the TA app

--- src/main/java/org/wise/portal/presentation/web/controllers/score/ScoreAPIController.java
+++ src/main/java/org/wise/portal/presentation/web/controllers/score/ScoreAPIController.java
@@ -4,20 +4,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * Controller for single-page SCORE app built with Angular
+ *
  * @author Anthony Perritano
  * @author Hiroki Terashima
  */
 @Controller
-@RequestMapping("/score-ta")
+@RequestMapping(value = { "/score-app" })
 public class ScoreAPIController {
 
+  /**
+   * Invokes the SCORE Teaching Assistant Tool based without a run
+   */
   @GetMapping
-  protected String showTeachingAssistant() {
+  protected String showTeachingAssistant(HttpServletRequest request, HttpServletResponse response) {
     return "forward:/score/teachingassistant/dist/index.html";
   }
 }
-
-
-  
