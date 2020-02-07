5dc05d0e4e7e51bd7271ae59f6070ae6ab121ed8
Anthony Perritano <aperritano@gmail.com>
2020-02-07 09:51:43 -0600

    added duration

--- src/main/resources/wise_db_init.sql
+++ src/main/resources/wise_db_init.sql
@@ -608,6 +608,7 @@ CREATE TABLE tasks
     endTime       datetime     DEFAULT NULL,
     complete      tinyint(1)   DEFAULT NULL,
     workgroupName varchar(255) DEFAULT NULL,
+    duration bigint(20) DEFAULT NULL,
     PRIMARY KEY (id)
 ) ENGINE = InnoDB
   AUTO_INCREMENT = 11
