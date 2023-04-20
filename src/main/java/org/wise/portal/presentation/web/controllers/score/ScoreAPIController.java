WISE-Community/WISE-API

updated package.json and afterbuild.js for ta app

src/main/java/org/wise/portal/presentation/web/controllers/score/ScoreAPIController.java
@@ -15,6 +15,11 @@ public class ScoreAPIController {
 
   @GetMapping(value = {"/teachingassistant", "/teachingassistant/**"})
   protected String showTeachingAssistant() {
-    return "forward:/score/teachingassistant/dist/index.html";
+    return "forward:/teachingassistant/dist/index.html";
+  }
+
+  @GetMapping(value = {"/test", "/test/**"})
+  protected String showTest() {
+    return "hello";
   }
 }

encorelab/SCORE-API

src/main/java/org/wise/portal/presentation/web/controllers/score/ScoreAPIController.java
@@ -13,8 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/score")
 public class ScoreAPIController {
 
-  @GetMapping(value = {"/teachingassistant", "/teachingassistant/**"})
+  @GetMapping(value = { "/teachingassistant", "/teachingassistant/**" })
   protected String showTeachingAssistant() {
-    return "forward:/score/teachingassistant/dist/index.html";
+    return "forward:/teachingassistant/dist/index.html";
   }
 }